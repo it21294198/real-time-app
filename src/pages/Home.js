53586f6a45e0d6a4f812d2553bde625b16fedb93
it21294198 <it21294198@my.sliit.lk>
2024-01-02 17:42:31 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ function Home() {
     return ( <div>
         <h1>In this project Database route &amp; Real Time data transmition using firebase RealTime db and Firestore is evaluated.</h1>
         <h2>The p5.js canvas shows</h2>
+        <h1>The Cloudinary image and video storage is here also</h1>
     </div> );
 }
 
